5408c0c0a24eb10a44c19f247b9321a7b98812a1
Gustavo Trindade <gustavoferreiratrindade@gmail.com>
2023-06-11 21:02:13 -0300

    projeto concluido

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,50 @@
+import Botao from "@/components/Botao";
+import Formulario from "@/components/Formulario";
 import Layout from "@/components/Layout";
 import Tabela from "@/components/Tabela";
-import Cliente from "@/core/Cliente";
+import useClientes from "@/hooks/useClientes";
 
 export default function Home() {
 
-  const clientes = [
-    new Cliente('Ana', 24,'1'),
-    new Cliente('Bia', 32,'2'),
-    new Cliente('Carlos', 16,'3'),
-    new Cliente('Marta', 19,'4'),
-    new Cliente('Carol', 45,'5'),
-  ]
+ const { 
+    cliente, 
+    clientes, 
+    novoCliente, 
+    salvarCliente,
+    selecionarCliente, 
+    excluirCliente, 
+    tabelaVisivel,
+    exibirTabela
+  } = useClientes()
+
   return (
-    <div className={`
-      flex 
-      h-screen 
-      justify-center 
-      items-center
-      bg-gradient-to-r
-      from-blue-500
-      to-purple-500
-      text-white`}>
+    <div className={` 
+      flex h-screen justify-center items-center
+      bg-gradient-to-r from-blue-500 to-purple-500 text-white
+    `}>
       <Layout titulo="CRUD com Nextjs e Reactjs">
-        <Tabela clientes></Tabela>
+        {tabelaVisivel ? (
+          <>
+            <div className="flex justify-end">
+              <Botao cor="green" className="mb-4"
+                onClick={novoCliente}
+              >
+                Novo Cliente
+              </Botao>
+            </div>
+            <Tabela 
+              clientes={clientes}
+              clienteSelecionado={selecionarCliente}
+              clienteExcluido={excluirCliente}
+            />
+          </>
+        ) : (
+          <Formulario 
+            cliente={cliente}
+            clienteMudou={salvarCliente}
+            cancelado={exibirTabela}
+          />
+        )}        
       </Layout>
     </div>
   )
